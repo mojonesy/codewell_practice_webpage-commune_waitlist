2cf46bf09aa063ee3e4855197af6196265154825
Molly <mojonesycreative@gmail.com>
2022-11-10 00:59:30 -0500

    responsive landing page

---
+++ public/index.js
@@ -0,0 +1,7 @@
+// const navToggle = document.querySelector(".nav-toggle");
+
+// navToggle.addEventListener("click", () => {
+//     document.body.classList.toggle("nav-open");
+// })
+
+// TODO: implement nav-toggle in CSS //
